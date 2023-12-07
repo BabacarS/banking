ac9eec0a18a4c4409d8a54828d4a0f98b0a187f7
Babacar <seck_93@hotmail.fr>
2023-12-07 12:26:08 +0000

    Second Commit

--- src/main/java/sn/btech/banking/models/User.java
+++ src/main/java/sn/btech/banking/models/User.java
@@ -1,16 +1,28 @@
 package sn.btech.banking.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @Builder
-@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue
     private Long id;
+    @NotNull
+    @Column(name="last_name",nullable = false)
+    private String lastName;
+    @NotNull
+    @Column(name="first_name",nullable = false)
+    private String firstName;
 }
